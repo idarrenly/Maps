acbbe697a3b3829d5981f01ca1c7d27f172f268f
dly <dly@mbp.local>
2017-10-14 17:05:56 -0700

    Simple use of MapKit with one default location with zoom.

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //Default location
+        let latitude: CLLocationDegrees = 37.35
+        let longitude: CLLocationDegrees = -121.95
+
+        //Zoom level
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        //Span-Coordinate-Region
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinate, span: span)
+        
+        //Set Map
+        mapView.setRegion(region, animated: true)
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
